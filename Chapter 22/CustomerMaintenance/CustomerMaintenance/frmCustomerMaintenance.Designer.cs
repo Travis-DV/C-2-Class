3160c5c1b2cbf79a5b0ccc92378ac18a7241dc33
Travis-DV <tfindley2@my.stlcc.edu>
2024-04-21 22:44:03 -0500

    Added Chapter 22

---
+++ Chapter 22/CustomerMaintenance/CustomerMaintenance/frmCustomerMaintenance.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace CustomerMaintenance;
+
+partial class frmCustomerMaintenance
+{
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        btnExit = new Button();
+        btnAdd = new Button();
+        DGVCustomers = new DataGridView();
+        BTFirst = new Button();
+        BTNext = new Button();
+        BTPrevious = new Button();
+        BTLast = new Button();
+        ((System.ComponentModel.ISupportInitialize)DGVCustomers).BeginInit();
+        SuspendLayout();
+        // 
+        // btnExit
+        // 
+        btnExit.Location = new Point(843, 303);
+        btnExit.Margin = new Padding(2);
+        btnExit.Name = "btnExit";
+        btnExit.Size = new Size(75, 23);
+        btnExit.TabIndex = 1;
+        btnExit.Text = "Exit";
+        btnExit.UseVisualStyleBackColor = true;
+        btnExit.Click += btnExit_Click;
+        // 
+        // btnAdd
+        // 
+        btnAdd.Location = new Point(16, 303);
+        btnAdd.Margin = new Padding(2);
+        btnAdd.Name = "btnAdd";
+        btnAdd.Size = new Size(75, 23);
+        btnAdd.TabIndex = 0;
+        btnAdd.Text = "Add";
+        btnAdd.UseVisualStyleBackColor = true;
+        btnAdd.Click += btnAdd_Click;
+        // 
+        // DGVCustomers
+        // 
+        DGVCustomers.AllowUserToAddRows = false;
+        DGVCustomers.AllowUserToDeleteRows = false;
+        DGVCustomers.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+        DGVCustomers.Location = new Point(12, 12);
+        DGVCustomers.Name = "DGVCustomers";
+        DGVCustomers.ReadOnly = true;
+        DGVCustomers.RowTemplate.Height = 25;
+        DGVCustomers.Size = new Size(905, 277);
+        DGVCustomers.TabIndex = 2;
+        DGVCustomers.CellClick += DGVCustomers_CellClick;
+        // 
+        // BTFirst
+        // 
+        BTFirst.Location = new Point(326, 306);
+        BTFirst.Name = "BTFirst";
+        BTFirst.Size = new Size(75, 23);
+        BTFirst.TabIndex = 3;
+        BTFirst.Text = "<<";
+        BTFirst.UseVisualStyleBackColor = true;
+        BTFirst.Click += BTFirst_Click;
+        // 
+        // BTNext
+        // 
+        BTNext.Location = new Point(488, 306);
+        BTNext.Name = "BTNext";
+        BTNext.Size = new Size(75, 23);
+        BTNext.TabIndex = 4;
+        BTNext.Text = ">";
+        BTNext.UseVisualStyleBackColor = true;
+        BTNext.Click += BTNext_Click;
+        // 
+        // BTPrevious
+        // 
+        BTPrevious.Location = new Point(407, 306);
+        BTPrevious.Name = "BTPrevious";
+        BTPrevious.Size = new Size(75, 23);
+        BTPrevious.TabIndex = 5;
+        BTPrevious.Text = "<";
+        BTPrevious.UseVisualStyleBackColor = true;
+        BTPrevious.Click += BTPrevious_Click;
+        // 
+        // BTLast
+        // 
+        BTLast.Location = new Point(569, 306);
+        BTLast.Name = "BTLast";
+        BTLast.Size = new Size(75, 23);
+        BTLast.TabIndex = 6;
+        BTLast.Text = ">>";
+        BTLast.UseVisualStyleBackColor = true;
+        BTLast.Click += BTLast_Click;
+        // 
+        // frmCustomerMaintenance
+        // 
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        CancelButton = btnExit;
+        ClientSize = new Size(929, 341);
+        Controls.Add(BTLast);
+        Controls.Add(BTPrevious);
+        Controls.Add(BTNext);
+        Controls.Add(BTFirst);
+        Controls.Add(DGVCustomers);
+        Controls.Add(btnAdd);
+        Controls.Add(btnExit);
+        FormBorderStyle = FormBorderStyle.FixedDialog;
+        Margin = new Padding(2);
+        Name = "frmCustomerMaintenance";
+        StartPosition = FormStartPosition.CenterScreen;
+        Text = "Customer Maintenance";
+        Load += frmCustomerDisplay_Load;
+        ((System.ComponentModel.ISupportInitialize)DGVCustomers).EndInit();
+        ResumeLayout(false);
+    }
+
+    #endregion
+    private Button btnExit;
+    private Button btnAdd;
+    private DataGridView DGVCustomers;
+    private Button BTFirst;
+    private Button BTNext;
+    private Button BTPrevious;
+    private Button BTLast;
+}
---
+++ Chapter 22/CustomerMaintenance/CustomerMaintenance/frmCustomerMaintenance.cs
@@ -0,0 +1,238 @@
+﻿using CustomerMaintenance.Models.DataLayer;
+
+namespace CustomerMaintenance;
+
+public partial class frmCustomerMaintenance : Form
+{
+    public frmCustomerMaintenance()
+    {
+        InitializeComponent();
+    }
+
+    private MMABooksDataAccess data = new();
+    private Customer selectedCustomer = null!;
+
+    // private constants for the index values of the Modify and Delete button columns
+    private const int ModifyIndex = 6;
+    private const int DeleteIndex = 7;
+    private const int MaxRows = 10;
+    private int totalRows = 0;
+    private int pages = 0;
+    private int pageNumber = 1;
+
+    private void frmCustomerDisplay_Load(object sender, EventArgs e)
+    {
+        totalRows = data.CustomerCount;
+
+        pages = totalRows / MaxRows;
+        if (totalRows % MaxRows != 0)
+        {
+            pages += 1;
+        }
+        pageNumber = 1;
+        DisplayCustomers();
+    }
+
+    private void DisplayCustomers()
+    {
+        // get customers and bind grid
+        DGVCustomers.Columns.Clear();
+        //DGVCustomers.DataSource = data.GetCustomers();
+
+        int skip = MaxRows * (pageNumber - 1);
+        int take = MaxRows;
+
+        if (pageNumber == pages)
+        {
+            take = totalRows - skip;
+        }
+        if (totalRows <= MaxRows)
+        {
+            take = totalRows;
+        }
+        DGVCustomers.DataSource = data.GetCustomers(skip, take);
+
+        // format grid
+        DGVCustomers.Columns[0].Visible = false;
+        DGVCustomers.Columns[1].Width = 150;
+        DGVCustomers.Columns[2].Width = 150;
+        DGVCustomers.Columns[5].HeaderText = "Zip Code";
+        DGVCustomers.AlternatingRowsDefaultCellStyle.BackColor = Color.Green;
+        DGVCustomers.EnableHeadersVisualStyles = false;
+        DGVCustomers.ColumnHeadersDefaultCellStyle.BackColor = Color.DarkGreen;
+        DGVCustomers.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+
+        DataGridViewButtonColumn modifyColumn = new()
+        {
+            UseColumnTextForButtonValue = true,
+            HeaderText = "",
+            Text = "Modify"
+        };
+        DGVCustomers.Columns.Insert(ModifyIndex, modifyColumn);
+
+        DataGridViewButtonColumn deleteColumn = new()
+        {
+            UseColumnTextForButtonValue = true,
+            HeaderText = "",
+            Text = "Delete"
+        };
+        DGVCustomers.Columns.Insert(DeleteIndex, deleteColumn);
+    }
+
+    private void  EnableDisableButtons()
+    {
+        if (pageNumber == 1)
+        {
+            BTFirst.Enabled = false;
+            BTPrevious.Enabled = false;
+        }
+        else
+        {
+            BTFirst.Enabled = true;
+            BTPrevious.Enabled = true;
+        }
+
+        if (pageNumber == pages)
+        {
+            BTNext.Enabled = false;
+            BTLast.Enabled = false;
+        }
+        else
+        {
+            BTNext.Enabled = true;
+            BTLast.Enabled = true;
+        }
+    }
+
+    private void ModifyCustomer()
+    {
+        frmAddModifyCustomer addModifyCustomerForm = new()
+        {
+            Customer = selectedCustomer,
+            States = data.GetStates()
+        };
+
+        DialogResult result = addModifyCustomerForm.ShowDialog();
+        if (result == DialogResult.OK)
+        {
+            try
+            {
+                selectedCustomer = addModifyCustomerForm.Customer;
+                data.UpdateCustomer(selectedCustomer);
+                DisplayCustomers();
+            }
+            catch (DataAccessException ex)
+            {
+                HandleDataAccessError(ex);
+            }
+        }
+    }
+
+    private void DeleteCustomer()
+    {
+        DialogResult result =
+            MessageBox.Show($"Delete {selectedCustomer.Name}?",
+            "Confirm Delete",
+            MessageBoxButtons.YesNo,
+            MessageBoxIcon.Question);
+        if (result == DialogResult.Yes)
+        {
+            try
+            {
+                data.RemoveCustomer(selectedCustomer);
+                DisplayCustomers();
+            }
+            catch (DataAccessException ex)
+            {
+                HandleDataAccessError(ex);
+            }
+        }
+    }
+
+    private void btnAdd_Click(object sender, EventArgs e)
+    {
+        frmAddModifyCustomer addModifyCustomerForm = new()
+        {
+            States = data.GetStates()
+        };
+        DialogResult result = addModifyCustomerForm.ShowDialog();
+        if (result == DialogResult.OK)
+        {
+            try
+            {
+                selectedCustomer = addModifyCustomerForm.Customer;
+                data.AddCustomer(selectedCustomer);
+                DisplayCustomers();
+            }
+            catch (DataAccessException ex)
+            {
+                HandleDataAccessError(ex);
+            }
+        }
+    }
+
+    private void HandleDataAccessError(DataAccessException ex)
+    {
+        // if concurrency conflict, re-display products
+        if (ex.IsConcurrencyError)
+        {
+            DisplayCustomers();
+        }
+
+        // display error message in dialog with error type as title
+        MessageBox.Show(ex.Message, ex.ErrorType);
+    }
+
+    private void btnExit_Click(object sender, EventArgs e)
+    {
+        this.Close();
+    }
+
+    private void DGVCustomers_CellClick(object sender, DataGridViewCellEventArgs e)
+    {
+        if (e.RowIndex > -1)
+        {
+            if (e.ColumnIndex == ModifyIndex || e.ColumnIndex == DeleteIndex)
+            {
+                int customerID = Convert.ToInt32(DGVCustomers.Rows[e.RowIndex].Cells[0].Value);
+                selectedCustomer = data.FindCustomer(customerID);
+            }
+
+            if (selectedCustomer != null)
+            {
+                if (e.ColumnIndex == ModifyIndex)
+                {
+                    ModifyCustomer();
+                }
+                else if (e.ColumnIndex == DeleteIndex)
+                {
+                    DeleteCustomer();
+                }
+            }
+        }
+    }
+
+    private void BTFirst_Click(object sender, EventArgs e)
+    {
+        pageNumber = 1;
+        DisplayCustomers();
+    }
+
+    private void BTPrevious_Click(object sender, EventArgs e)
+    {
+        pageNumber--;
+        DisplayCustomers();
+    }
+
+    private void BTNext_Click(object sender, EventArgs e)
+    {
+        pageNumber++;
+        DisplayCustomers();
+    }
+
+    private void BTLast_Click(object sender, EventArgs e)
+    {
+        pageNumber = pages;
+        DisplayCustomers();
+    }
+}
